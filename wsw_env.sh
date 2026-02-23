16f37eb60aeb6dd7b844e1d80976d3e5cb08ea7f
mindulmindul <mindul@qq.com>
2026-02-23 20:32:25 +0800

    more better traceback and error-code

--- wsw_env.sh
+++ wsw_env.sh
@@ -1,179 +1,235 @@
-rscur () {
-    cur_dir=${${PWD}#$(css)/}
-    echo '==>' cur_dir is ${cur_dir}
-    if [[ -e ${PWD}/.git ]]; then
-        echo '==>' repo sync -c ${cur_dir}
-        #repo sync -c ${cur_dir}
-        return $?
-    fi
-    echo '==>' repo sync --force-sync -d -c $(repo manifest | rg --pcre2 "(?<=path=\")${cur_dir}[^\"]+(?=\")" | rg --pcre2 -o "(?<=name=\")[^\"]+?(?=\")" ) -j$(nproc)
-    #repo sync --force-sync -d -c $(repo manifest | rg --pcre2 "(?<=path=\")[^\"]+(?=\")" | rg --pcre2 -o "(?<=name=\")[^\"]+?(?=\")" ) -j$(nproc)
-}
-
-rs () {
-    tmp_xml=$(mktemp).xml
-    repo manifest -o ${tmp_xml}
-    # TODO:后续要把 repo manifest -Ro 实现，创造自己的repo仓
-    repo sync -c -j$(grep 'processor' /proc/cpuinfo | wc -l) --force-sync --force-checkout -d -m ${tmp_xml}
-}
-
-cdd () {
-    if [[ $# -lt 1 || $# -gt 1 ]]; then
-        echo "Usage: cdd TARGET" >&2
-        return -1
-    fi
-    # 如果有一个参数
-    ## 首先判断参数是不是一个路径或者文件
-    if [[ -e $1 ]]; then
-        if [[ -f $1 ]]; then
-            cd $(dirname $1)
-            return 0
-        elif [[ -d $1 ]]; then
-            cd $1
-            return 0
-        fi
-    fi
-    ## 然后判断当前是否在repo仓库中，判断参数是否对应一个仓库名或者仓库对应的路径
-    ## 是则直接跳到对应的仓库路径下，不是则return 1
-    target=$1
-    if [[ -n $(css) ]]; then
-        p=$(repo manifest 2>/dev/null | grep "path=\"${target}\"")
-        if [[ -n ${p} ]]; then
-            cs; cd ${target}
-            return 0
-        fi
-        n=$(repo manifest 2>/dev/null | grep "name=\"${target}\"")
-        if [[ -n ${n} ]]; then
-            p=$(repo manifest 2>/dev/null | grep "${target}" | sed -nE 's#.*path="([^"]+)".*$#\1#p')
-            if [[ -n ${p} ]]; then
-                cs; cd ${p}
-                return 0
-            else
-                cs; cd ${n}
-                return 0
-            fi
-        fi
-    fi
-    return -1
-}
-cur_dir=$(get_this_dir)
-REPO_QUERY_PY="${cur_dir}/my_repo.py"
-
-# 1. 根据路径拿名字
-repo_mfst_get_name_from_path() {
-    local rel=${$(ctt)#$(css)/}
-    $REPO_QUERY_PY name_from_path "$rel" --root "$(css)"
-}
-
-# 2. 根据名字拿路径
-repo_mfst_get_path_from_name() {
-    $REPO_QUERY_PY path_from_name "$1" --root "$(css)"
-}
-
-# 3. 根据路径拿分支 (最核心的同步逻辑)
-repo_mfst_get_branch_from_path() {
-    local rel=${$(ctt)#$(css)/}
-    $REPO_QUERY_PY branch_from_path "$rel" --root "$(css)"
-}
-
-# 4. 根据名字拿分支
-repo_mfst_get_branch_from_name() {
-    $REPO_QUERY_PY branch_from_name "$1" --root "$(css)"
-}
-
-# 5. 根据名字拿remote
-repo_mfst_get_remote_from_name() {
-    $REPO_QUERY_PY remote_from_name "$1" --root "$(css)"
-}
-
-# 6. 根据路径拿remote
-repo_mfst_get_remote_from_path() {
-    $REPO_QUERY_PY remote_from_path "$1" --root "$(css)"
-}
+export WSW_ANDROID_DIR=$(get_this_dir)
 
 cs ()
 {
+    local dir
     dir=$(_up_to_have_dir .repo)
     if [[ $? -eq 0 ]]; then
         cd ${dir}
         return 0
+    else
+        echo "${funcstack[1]}: not in repo dir!!!" >&2
+        return 1
     fi
-    echo "not in repo dir!!!"
-    return 1
 }
 
 css ()
 {
+    local dir
     dir=$(_up_to_have_dir .repo)
     if [[ $? -eq 0 ]]; then
         echo ${dir}
         return 0
+    else
+        echo "${funcstack[1]}: not in repo dir!!!" >&2
+        return 1
     fi
-    echo "not in repo dir!!!"
-    return 1
 }
 
 ct ()
 {
+    local dir
     dir=$(_up_to_have_dir .git)
     if [[ $? -eq 0 ]]; then
         cd ${dir}
         return 0
+    else
+        echo "${funcstack[1]}: not in git dir!!!" >&2
+        return 1
     fi
-    echo "not in git dir!!!"
-    return 1
 }
 
 ctt ()
 {
+    local dir
     dir=$(_up_to_have_dir .git)
     if [[ $? -eq 0 ]]; then
         echo ${dir}
         return 0
+    else
+        echo "${funcstack[1]}: not in git dir!!!" >&2
+        return 1
     fi
-    echo "not in git dir!!!"
-    return 1
 }
 
 cnp ()
 {
-    dir=${$(ctt)#$(css)/}
-    if [[ $? -eq 0 ]]; then
-        echo ${dir}
-        return 0
-    fi
-    echo "not in repo dir!!!"
-    return 1
+    local dir ctt_dir css_dir
+    {
+        ctt_dir=$(ctt) &&
+        css_dir=$(css)
+    } || {
+        echo "${funcstack[1]}: not in git dir!!!" >&2
+        return 1
+    }
+    dir=${${ctt_dir}##${css_dir}/}
+    echo ${dir}
+    return 0
 }
 
-cnn ()
-{
-    repo_path=$(cnp)
-    if [[ $? -ne 0 ]]; then
+cnn() {
+    local cnp_dir css_dir
+    {
+        css_dir=$(css) &&
+        cnp_dir=$(cnp)
+    } || {
+        echo "${funcstack[1]}: not in git dir!!!" >&2
         return 1
-    fi
-    echo $(repo_mfst_get_name_from_path ${repo_path})
+    }
+    {
+        ${WSW_ANDROID_DIR}/my_repo.py name_from_path ${cnp_dir} --root ${css_dir}
+    } || {
+        echo "${funcstack[1]}: ${WSW_ANDROID_DIR}/my_repo.py: no project with path:'${repo_path}' in your manifests!!!" >&2
+        return 1
+    }
+    return 0
+}
+
+# 1. 根据路径拿名字
+repo_mfst_get_name_from_path() {
+    local repo_path=$1
+    local css_dir
+    {
+        css_dir=$(css)
+    } || {
+        echo "${funcstack[1]}: not in repo dir!!!" >&2
+        return 1
+    }
+    {
+        ${WSW_ANDROID_DIR}/my_repo.py name_from_path ${repo_path} --root ${css_dir}
+    } || {
+        echo "${funcstack[1]}: ${WSW_ANDROID_DIR}/my_repo.py: no project with path:'${repo_path}' in your manifests!!!" >&2
+        return 1
+    }
+    return 0
+}
+
+
+# 2. 根据名字拿路径
+repo_mfst_get_path_from_name() {
+    local repo_name=$1
+    local css_dir
+    {
+        css_dir=$(css)
+    } || {
+        echo "${funcstack[1]}: not in repo dir!!!" >&2
+        return 1
+    }
+    {
+        ${WSW_ANDROID_DIR}/my_repo.py path_from_name ${repo_name} --root ${css_dir}
+    } || {
+        echo "${funcstack[1]}: ${WSW_ANDROID_DIR}/my_repo.py: no project with name:'${repo_name}' in your manifests!!!" >&2
+        return 1
+    }
+    return 0
+}
+
+# 3. 根据路径拿分支
+repo_mfst_get_branch_from_path() {
+    local repo_path=$1
+    local css_dir
+    {
+        css_dir=$(css)
+    } || {
+        echo "${funcstack[1]}: not in repo dir!!!" >&2
+        return 1
+    }
+    {
+        ${WSW_ANDROID_DIR}/my_repo.py branch_from_path ${repo_path} --root ${css_dir}
+    } || {
+        echo "${funcstack[1]}: ${WSW_ANDROID_DIR}/my_repo.py: no project with path:'${repo_path}' in your manifests!!!" >&2
+        return 1
+    }
+    return 0
+}
+
+# 4. 根据名字拿分支
+repo_mfst_get_branch_from_name() {
+    local repo_name=$1
+    local css_dir
+    {
+        css_dir=$(css)
+    } || {
+        echo "${funcstack[1]}: not in repo dir!!!" >&2
+        return 1
+    }
+    {
+        ${WSW_ANDROID_DIR}/my_repo.py branch_from_name ${repo_name} --root ${css_dir}
+    } || {
+        echo "${funcstack[1]}: ${WSW_ANDROID_DIR}/my_repo.py: no project with name:'${repo_name}' in your manifests!!!" >&2
+        return 1
+    }
+    return 0
+}
+
+# 5. 根据名字拿remote
+repo_mfst_get_remote_from_name() {
+    local repo_name=$1
+    local css_dir
+    {
+        css_dir=$(css)
+    } || {
+        echo "${funcstack[1]}: not in repo dir!!!" >&2
+        return 1
+    }
+    {
+        ${WSW_ANDROID_DIR}/my_repo.py remote_from_name ${repo_name} --root ${css_dir}
+    } || {
+        echo "${funcstack[1]}: ${WSW_ANDROID_DIR}/my_repo.py: no project with name:'${repo_name}' in your manifests!!!" >&2
+        return 1
+    }
+    return 0
+}
+
+# 6. 根据路径拿remote
+repo_mfst_get_remote_from_path() {
+    local repo_path=$1
+    local css_dir
+    {
+        css_dir=$(css)
+    } || {
+        echo "${funcstack[1]}: not in repo dir!!!" >&2
+        return 1
+    }
+    {
+        ${WSW_ANDROID_DIR}/my_repo.py remote_from_path ${repo_path} --root ${css_dir}
+    } || {
+        echo "${funcstack[1]}: ${WSW_ANDROID_DIR}/my_repo.py: no project with path:'${repo_path}' in your manifests!!!" >&2
+        return 1
+    }
     return 0
 }
 
 cnb ()
 {
-    repo_path=$(cnp)
-    if [[ $? -ne 0 ]]; then
+    local repo_path
+    {
+        repo_path=$(cnp)
+    } || {
+        echo "${funcstack[1]}: not in git dir!!!" >&2
         return 1
-    fi
-    echo $(repo_mfst_get_branch_from_path ${repo_path})
-    return 0
+    }
+    {
+        echo $(repo_mfst_get_branch_from_path ${repo_path})
+    } || {
+        echo "${funcstack[1]}: param wrong !!!" >&2
+    }
 }
 
 cnr ()
 {
-    repo_path=$(cnp)
-    if [[ $? -ne 0 ]]; then
+    local repo_path
+    {
+        repo_path=$(cnp)
+    } || {
+        echo "${funcstack[1]}: not in git dir!!!" >&2
         return 1
-    fi
-    echo $(repo_mfst_get_remote_from_path ${repo_path})
-    return 0
+    }
+    {
+        echo $(repo_mfst_get_remote_from_path ${repo_path})
+    } || {
+        echo "${funcstack[1]}: param wrong !!!" >&2
+    }
 }
 
 cm ()
@@ -183,6 +239,7 @@ cm ()
         cd ${dir}/.repo/manifests
         return 0
     fi
+    echo "${funcstack[1]}: not in repo dir!!!" >&2
     return 1
 }
 
@@ -193,21 +250,54 @@ cmm ()
         echo ${dir}/.repo/manifests
         return 0
     fi
+    echo "${funcstack[1]}: not in repo dir!!!" >&2
     return 1
 }
+
+# only used by zsh
+cdd () {
+    if [[ $# -ne 1 ]]; then
+        echo "Usage: cdd TARGET" >&2
+        return 1
+    fi
+    local target=$1
+    if [[ -e $target ]]; then
+        if [[ -f $target ]]; then
+            cd ${target:h}
+            return 0
+        elif [[ -d $target ]]; then
+            cd $target
+            return 0
+        fi
+    fi
+    # for now, target might be a a repo name or path
+    # use repo manifest parser to transform it to abs path
+    local repo_path repo_name css_dir repo_abs_path
+    {
+        css_dir=$(css) &&
+        { repo_path=$(repo_mfst_get_path_from_name ${target}) ||
+            { repo_name=$(repo_mfst_get_name_from_path ${target}) && repo_path=${target}; }
+        }
+    } && {
+        repo_abs_path=${css_dir}/${repo_path}
+        [[ -d ${repo_abs_path} ]] && cd ${repo_abs_path} && return 0;
+    }
+    echo "${funcstack[1]}: not in repo dir!!!" >&2
+    return -1
+}
+
 
 alias gb > /dev/null 2>&1 && unalias gb || true
 gb ()
 {
-    # 从project标签中拿到project指定的branch和remote
-    ctt &>/dev/null
-    if [[ $? -ne 0 ]]; then
-        echo "not in repo dir!!!"
+    {
+        ctt_dir=$(ctt) &&
+        repo_branch=$(cnb) &&
+        repo_remote=$(cnr)
+    } || {
+        echo "${funcstack[1]}: not in project dir!!!" >&2
         return 1
-    fi
-    repo_branch=$(cnb)
-    repo_remote="$(cnr)"
-
+    }
     echo "git push ${repo_remote} HEAD:refs/for/${repo_branch}"
     echo "git push ${repo_remote} HEAD:${repo_branch}"
     echo "git pull ${repo_remote} ${repo_branch} --unshallow"
@@ -217,27 +307,56 @@ gb ()
     echo "remote: ${repo_remote}"
     echo "branch: ${repo_branch}"
     git remote -v
+    return $?
 }
 
-gfun ()
-{
-    gb | awk 'NR==5 {print; exit}'
-}
-
-gpun ()
-{
-    gb | awk 'NR==3 {print; exit}'
-}
-
-# 直接推送到远端，注意如果是在公司需要gerrit审核，这里要改成第1行的命令
+# 直接推送到远端，注意如果是在公司需要gerrit审核，need_to_review需要设置成0
+# `need_to_review` means need to push HEAD to refs/for/BRANCH nor BRANCH
 gbb ()
 {
-    if [[ -f ./gbb ]]; then
-        zsh ./gbb
-    else
-        cmd=$(gb | head -2 | tail -1)
-        echo "${cmd}"
-        eval ${cmd}
+    local ctt_dir cmd need_to_review=1
+    if ! ctt_dir=$(ctt); then
+        return 1
     fi
+
+    if [[ -f ${ctt_dir}/gbb ]]; then
+        zsh ${ctt_dir}/gbb
+        return 0
+    fi
+    cmd=$(gb) || echo "${funcstack[1]}: not in project dir!!!" >&2 && return 1
+    if [[ ${need_to_review} -eq 0 ]]; then
+        cmd=$(grep -P 'git push .*?HEAD:(refs/for/)' <(echo ${cmd}))
+    else
+        cmd=$(grep -P 'git push .*?HEAD:(?!refs/for/)' <(echo ${cmd}))
+    fi
+    echo ${cmd}
+    eval ${cmd}
+    return $?
+}
+
+rscur () {
+# repo sync all repo project in current rel-path
+    [[ -z "$(css)" ]] && return 0
+
+    local _cur_dir="" _answer="" _cmd=""
+    _cur_dir=${${PWD}#$(css)/}
+    if [[ -n "$(cnn)" ]]; then
+        _cmd="repo sync -c ${cur_dir}"
+    else
+        _cmd="repo sync --force-sync -d -c $(repo manifest | rg --pcre2 '(?<=path=\")${cur_dir}[^\"]+(?=\")' | rg --pcre2 -o '(?<=name=\")[^\"]+?(?=\")" ') -j$(nproc)"
+    fi
+    echo '==>'"${_cmd}"
+    printf "execute it? ({ENTER/Y/y}/{N/n/*}) "
+    read answer
+    [[ -z "${answer}" ]] || [[ "${answer}" =~ ^(Y|y)?$ ]] && eval "${_cmd}"
+
+    unset _cur_dir _answer _cmd
+}
+
+rs () {
+    tmp_xml=$(mktemp).xml
+    repo manifest -o ${tmp_xml}
+    # TODO:后续要把 repo manifest -Ro 实现，创造自己的repo仓
+    repo sync -c -j$(grep 'processor' /proc/cpuinfo | wc -l) --force-sync --force-checkout -d -m ${tmp_xml}
 }
 
